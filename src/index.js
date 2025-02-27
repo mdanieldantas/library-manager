5ffbb05c8d10a879c900787c2fbec89e313967de
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-02-27 17:46:01 -0300

    adiciona suporte a CORS, atualiza porta do servidor e cria página de busca de livros por autor

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ require("dotenv").config();
 const authRouter  = require("./routes/auth");
 const apiRouter = require("./routes/api");
 const express = require("express");
+const cors = require("cors");
+
+
+
 const app = express();
+// Habilitar CORS para todas as origens
+app.use(cors());
+
 
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/api", apiRouter);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
